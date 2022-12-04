1b5e82bc66fb6532ccd8d2bd6f594c205ba0c865
MathisGirardin <93118154+MathisGirardin@users.noreply.github.com>
2022-12-04 16:17:30 +0100

    simple commentary of main.cpp

--- src/MBSensor/source/main.cpp
+++ src/MBSensor/source/main.cpp
@@ -28,16 +28,19 @@ DEALINGS IN THE SOFTWARE.
 #include "veml6070.h"
 #include "tsl256x.h"
 
-MicroBit uBit;
-MicroBitI2C i2c(I2C_SDA0,I2C_SCL0);
-MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_DIGITAL_OUT);
+MicroBit uBit; //Sensor Microbit
+MicroBitI2C i2c(I2C_SDA0,I2C_SCL0); // I2C port for data communication
+MicroBitPin P0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_DIGITAL_OUT); //P0 for Rst link
 
-ManagedString order = "T";
+ManagedString format = "T"; // Printing format: Default is temperature only
 
-const int KEY_CRYPT = 8;
-int KEY_UNCRYPT = (KEY_CRYPT * -1);
+//OLED variables
 const uint8_t NUMBER_OF_LINE_SCREEN = 8;
 const uint8_t NUMBER_OF_COL_SCREEN = 16;
+
+// Decryption
+const int KEY_CRYPT = 8;
+int KEY_UNCRYPT = (KEY_CRYPT * -1);
 
 ManagedString shift(int key, ManagedString text) {
     ManagedString result = "";
@@ -50,11 +53,13 @@ ManagedString shift(int key, ManagedString text) {
     return result;
 }
 
+//Receive format from Gateway Microbit
 void onData(MicroBitEvent)
 {
-    order = shift(KEY_UNCRYPT, uBit.radio.datagram.recv());
+    format = shift(KEY_UNCRYPT, uBit.radio.datagram.recv());
 }
 
+//Check if the line is displayable before displaying it
 void displayLine(ssd1306* screen, uint8_t line, uint8_t col, const char* text)
 {
     if (line < NUMBER_OF_LINE_SCREEN && line >= 0 && col < NUMBER_OF_COL_SCREEN && col >= 0){
@@ -66,13 +71,16 @@ int main()
 {
     // Initialise the micro:bit runtime.
     uBit.init();
+    
+    //Enable radio communication
     uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
-
     uBit.radio.enable();
     uBit.radio.setGroup(8);
-
+    
+    //Temporary variable to
     char tmp[10];
-
+    
+    //Sensors data
     ManagedString temperature;
     ManagedString humidity;
     ManagedString pressure;
@@ -80,23 +88,31 @@ int main()
     ManagedString ir;
     ManagedString lux;
 
+    //Data sent to Gateway
     ManagedString allValues;
 
+    //OLED Screen
     ssd1306 screen(&uBit, &i2c, &P0);
+    
+    //Sensors
+    //BME280
     bme280 bme(&uBit,&i2c);
     uint32_t presValue = 0;
     int32_t tempValue = 0;
     uint16_t humiValue = 0;
     
+    //VEML6070
     veml6070 veml(&uBit,&i2c);
     uint16_t uvValue = 0;
 
+    //TSL256X
     tsl256x tsl(&uBit,&i2c);
     uint16_t combValue =0;
     uint16_t irValue = 0;
     uint32_t luxValue = 0;
     
     while(1){
+        //Read BME
         bme.sensor_read(&presValue, &tempValue, &humiValue);
         sprintf(tmp, "%d.%d", bme.compensate_temperature(tempValue) / 100, bme.compensate_temperature(tempValue) % 100);
         temperature = ManagedString(tmp); 
@@ -105,26 +121,32 @@ int main()
         sprintf(tmp, "%d", bme.compensate_pressure(presValue)/100);
         pressure = ManagedString(tmp);
         
+        //READ VEML
         veml.sensor_read(&uvValue);
         uv = ManagedString((int)uvValue);
 
+        //READ TSL
         tsl.sensor_read(&combValue, &irValue, &luxValue);
         ir = ManagedString((int)irValue);
         lux = ManagedString((int)luxValue);
         
+        //Send data to Gateway
         allValues = temperature + "|" + humidity + "|" + pressure + "|" + lux + "|" + ir + "|" + uv;
         uBit.radio.datagram.send(allValues);
 
-        int j = 0;
+        //Change data to its printing format
         temperature = temperature + " C";
         humidity = "Humidity: " + humidity;
         pressure = "Pressure: " + pressure + " hPa";
         uv = "UV: " + uv + " unit";
         ir = "IR: " + ir + " unit";
         lux = "Lux: " + lux + " unit";
-
-        for (int i = 0; i < order.length(); i++){
-            switch (order.charAt(i))
+        
+        int j = 0;  //Number of line printed
+        
+        //Display data according to format
+        for (int i = 0; i < format.length(); i++){
+            switch (format.charAt(i))
             {
             case 'T':
                 displayLine(&screen, j++, 0, "Temperature: ");
@@ -149,6 +171,8 @@ int main()
                 break;
             }
         }
+        
+        //Display error message
         if (j > NUMBER_OF_LINE_SCREEN){
             displayLine(&screen, 0, 0, "Format too long");
             for (int i = 1; i < NUMBER_OF_LINE_SCREEN; i++){
@@ -158,8 +182,8 @@ int main()
         screen.update_screen();
         uBit.sleep(1000);
     }
+    
     uBit.radio.disable();
-
     release_fiber();
 }
 
