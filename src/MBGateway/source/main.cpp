ecdab4e1de23ccd67f7b8dec8de9284b4b6039ec
R-Larue <93131994+R-Larue@users.noreply.github.com>
2022-12-04 12:42:41 +0100

    Comments, comments, comments
    In english, please !

--- src/MBGateway/source/main.cpp
+++ src/MBGateway/source/main.cpp
@@ -3,23 +3,24 @@
 #include <stdlib.h>
 #include "MicroBit.h"
 
-MicroBit uBit;                          //Bah uBit, quoi...
-MicroBitSerial serial(USBTX, USBRX);    //Initialisation du port Serial
-const int KEY_CRYPT = 8;                //Clé de chiffrage
-int KEY_UNCRYPT = (KEY_CRYPT * -1);     //Clé de déchiffrage
+MicroBit uBit;                          
+MicroBitSerial serial(USBTX, USBRX);    // Serial Port initialization
+const int KEY_CRYPT = 8;                // Encryption key
+int KEY_UNCRYPT = (KEY_CRYPT * -1);     // Decryption key
 
-ManagedString SerialRec; //Dernier message recu par le port UART USB 
+ManagedString SerialRec; // Last message received on port UART USB 
 
-//Animation de stand-by
+// Stand-by animation
 MicroBitImage load("0 0 1 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0\n0 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0\n0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n");
 
-//Animation de reception/envoi de données
+// Data sent/received animation
 MicroBitImage down("0,0,255,0,0\n0,0,0,255,0\n255,255,255,255,255\n0,0,0,255,0\n0,0,255,0,0\n");
 
-/// @brief Encodage ASCII-Shift
-/// @param key Clé d'encodage
-/// @param text Texte a encoder
-/// @return Texte encodé
+/// @brief ASCII-Shift encoding
+/// Each letter in the message is replaced by a character [key] number of position further in the ascii alphabet
+/// @param key Encryption key
+/// @param text about to be encrypted
+/// @return Encrypted text
 ManagedString shift(int key, ManagedString text) {
     ManagedString result = "";
     char ascii;
@@ -31,71 +32,70 @@ ManagedString shift(int key, ManagedString text) {
     return result;
 }
 
-/// @brief Envoi de données au capteurs micro:bit
-/// @param message Message à envoyer
+/// @brief Sends data to the sensor module
+/// @param message Data to send to the module
 void sendToMBSensor(ManagedString message) {
-    uBit.display.scrollAsync(message, 40);                  //Affichage du message envoyé
-    ManagedString messageCrypt = shift(KEY_CRYPT, message); //Chiffrage du message
-    ManagedString toSend(messageCrypt);                     
-    uBit.radio.datagram.send(toSend);                       //Envoi du message par Radio Tranceiver
+    uBit.display.scrollAsync(message, 40);                  // Scrolls message to send on Microbit Gateway
+    ManagedString messageCrypt = shift(KEY_CRYPT, message); // Encrypts message before sending it
+    ManagedString toSend(messageCrypt);                     // Store message in a ManagedString
+    uBit.radio.datagram.send(toSend);                       // Sends message to sensor module
 }
 
-/// @brief Evenement lors de l'appui sur un des deux boutons micro:bit.  
-///Il s'agit de debug et failsafe en cas de probleme d'envoi de données aux capteurs
+/// @brief Event when a micro:bit button is pressed
+/// This function is not needed for the project but comes in handy fairly often when debugging, to avoid going through the whole process
 /// @param  
 void onButton(MicroBitEvent)
 {   
     ManagedString message;
     if (uBit.buttonA.isPressed()) { 
-        message = "PT"; //Force l'affichage des capteurs en mode PRESSURE/TEMPERATURE
+        message = "PT"; // Sensors PRESSURE/TEMPERATURE mode
     }
     if (uBit.buttonB.isPressed()) { 
-        message = "TH"; //Force l'affichage des capteurs en mode TEMPERATURE/HUMIDITY
+        message = "TH"; // Sensors TEMPERATURE/HUMIDITY mode
     }
     sendToMBSensor(message);
 }
 
 
-/// @brief Evenement de réception de données via le Radio Receiver 
+/// @brief Data received from Radio Receiver event 
 /// @param  
 void onData(MicroBitEvent) {
-    ManagedString s = uBit.radio.datagram.recv();           //Reception du packet de données radio
-    uBit.display.stopAnimation();                           //Affiche l'animation de reception (pins vers usb)
-    uBit.display.rotateTo(MICROBIT_DISPLAY_ROTATION_90);
-    uBit.display.scrollAsync(down, 25 , 1);
-    // uBit.display.scrollAsync(s, 40);
-    ManagedString toSend(s + "\n");                         //Parse le string a envoyer
-    serial.send(toSend);                                    //Envoi de données par Serial UART USB
+    ManagedString s = uBit.radio.datagram.recv();           // Reception of radio datagram package
+    uBit.display.stopAnimation();                           // Stops current display to show "data received animation"
+    uBit.display.rotateTo(MICROBIT_DISPLAY_ROTATION_90);    // Animation goes from pins to usb
+    uBit.display.scrollAsync(down, 25 , 1);                 
+    ManagedString toSend(s + "\n");                         // Parse string to send
+    serial.send(toSend);                                    // Sends string through Serial UART USB
 }
 
-/// @brief Evenement de réception de données via la connexion Serial UART USB
+/// @brief Data received from Serial UART USB event
 /// @param  
 void onSerial(MicroBitEvent) {
-    SerialRec = serial.readUntil(ManagedString ("\r\n"),ASYNC); //Reception du message par l'UART jusqu'a obtenir un des caractères parmis \r et \n
-    uBit.display.stopAnimation();                               //Animation de réception (usb vers pins)
-    uBit.display.rotateTo(MICROBIT_DISPLAY_ROTATION_270);
+    SerialRec = serial.readUntil(ManagedString ("\r\n"),ASYNC); // Reads message from UART until \r or \n is received
+    uBit.display.stopAnimation();                               // Stops current display to show "data received animation"
+    uBit.display.rotateTo(MICROBIT_DISPLAY_ROTATION_270);       // Animation goes from pins to usb
     uBit.display.scrollAsync(down, 25 , 1);
-    serial.send(SerialRec+" ACK\n");                            //Renvoi en Serial + ACK
-    sendToMBSensor(SerialRec);                                  //Envoi de la commande aux capteurs micro:bit 
+    serial.send(SerialRec+" ACK\n");                            // Sends received message back to Serial + ACK
+    sendToMBSensor(SerialRec);                                  // Sends received message to sensor module
 }
 
-/// @brief Point d'entrée du programme
-/// @return Code d'erreur
+/// @brief Entry point
+/// @return Error code
 int main() { 
-    uBit.init();                    //Init des services micro:bit
-    serial.baud(115200);            //Init du port UART avec un baudrate maximum
-    serial.send("Loading...\n");    //Envoi d'un message part UART
-    uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);         //Event de réception de message radio
-    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_EVT_ANY, onButton);               //Event d'appui sur le bouton A
-    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_EVT_ANY, onButton);               //Event d'appui sur le bouton B
-    uBit.messageBus.listen(MICROBIT_ID_SERIAL, MICROBIT_SERIAL_EVT_DELIM_MATCH, onSerial);  //Event de réception de message UART
+    uBit.init();                    // Initialize micro:bit services
+    serial.baud(115200);            // Initialize UART port with set maximum baudrate
+    serial.send("Loading...\n");    // Sends serial message to signal beginning of setup sequence
+    uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);         // Radio data received event
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_EVT_ANY, onButton);               // A button pushed event (debug)
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_EVT_ANY, onButton);               // B button pushed event (debug)
+    uBit.messageBus.listen(MICROBIT_ID_SERIAL, MICROBIT_SERIAL_EVT_DELIM_MATCH, onSerial);  // UART data received event
 
-    uBit.radio.enable();                            //Activation du module radio
-    uBit.radio.setGroup(8);                         //Utilisation du groupe 8 pour la radio
-    serial.eventOn(ManagedString("\n") , ASYNC);    //Declenchement d'un event MICROBIT_SERIAL_EVT_DELIM_MATCH lors de la réception d'un caractère \n sur l'UART
-    serial.read(ASYNC);                             //Active la lecture sur le port UART (sans cette ligne eventOn n'est jamais déclenché)
-    serial.send("micro:bit READY\n");               //La Gateway est enfin prete à l'utilisation
+    uBit.radio.enable();                            // Enable radio
+    uBit.radio.setGroup(8);                         // Connection to group 8 for Radio
+    serial.eventOn(ManagedString("\n") , ASYNC);    // Launch MICROBIT_SERIAL_EVT_DELIM_MATCH event whenever \n is read on UART
+    serial.read(ASYNC);                             // Allow micro:bit to read on UART port (Otherwise eventOn is never reached)
+    serial.send("micro:bit READY\n");               // Sends serial message to signal micro:bit is ready
     while(1) 
-    { uBit.display.animate(load, 50 , 5); }         //Animation idle indiquant que la Gateway attend un message
-    release_fiber();                                //Fermeture de toutes les threads
+    { uBit.display.animate(load, 50 , 5); }         // Idle animation "awaiting message..."
+    release_fiber();                                // Closes all threads
 }
